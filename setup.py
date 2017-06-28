222fac4e1269063a03f2ab8e329d08dbc5a40cc6
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-06-28 18:04:35 -0400

    Rebase from 'debian/r2b2/examples_rclpy_minimal_subscriber'

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+from ament_python.data_files import get_data_files
 from ament_python.script_dir import install_scripts_to_libexec
 from setuptools import setup
 
 package_name = 'examples_rclpy_minimal_subscriber'
+data_files = get_data_files(package_name)
 install_scripts_to_libexec(package_name)
 
 setup(
@@ -12,6 +14,7 @@ setup(
         'subscriber_old_school',
         'subscriber_lambda',
         'subscriber_member_function'],
+    data_files=data_files,
     install_requires=['setuptools'],
     author='Mikael Arguedas',
     author_email='Mikael@osrfoundation.org',
@@ -29,9 +32,9 @@ setup(
     test_suite='test',
     entry_points={
         'console_scripts': [
-            'examples_rclpy_minimal_subscriber_old_school = subscriber_old_school:main',
-            'examples_rclpy_minimal_subscriber_lambda = subscriber_lambda:main',
-            'examples_rclpy_minimal_subscriber_member_function = subscriber_member_function:main',
+            'subscriber_old_school = subscriber_old_school:main',
+            'subscriber_lambda = subscriber_lambda:main',
+            'subscriber_member_function = subscriber_member_function:main',
         ],
     },
 )

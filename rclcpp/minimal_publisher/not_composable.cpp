36753c25cc0dcdc65048924c43b5a6a4ca4b7aa8
Michael Carroll <michael@openrobotics.org>
2019-05-08 19:35:37 -0500

    Rebase from 'upstream'

--- rclcpp/minimal_publisher/not_composable.cpp
+++ rclcpp/minimal_publisher/not_composable.cpp
@@ -28,14 +28,14 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("minimal_publisher");
-  auto publisher = node->create_publisher<std_msgs::msg::String>("topic");
-  auto message = std::make_shared<std_msgs::msg::String>();
+  auto publisher = node->create_publisher<std_msgs::msg::String>("topic", 10);
+  std_msgs::msg::String message;
   auto publish_count = 0;
   rclcpp::WallRate loop_rate(500ms);
 
   while (rclcpp::ok()) {
-    message->data = "Hello, world! " + std::to_string(publish_count++);
-    RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message->data.c_str());
+    message.data = "Hello, world! " + std::to_string(publish_count++);
+    RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();

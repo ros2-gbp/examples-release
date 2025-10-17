e774d5fc7099001c7184ab5475455f695c39be3a
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-17 12:08:21 +0200

    Rebase from 'upstream'

--- launch_testing/launch_testing_examples/setup.py
+++ launch_testing/launch_testing_examples/setup.py
@@ -4,7 +4,7 @@ package_name = 'launch_testing_examples'
 
 setup(
     name=package_name,
-    version='0.19.6',
+    version='0.19.7',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -17,7 +17,11 @@ setup(
     maintainer_email='aditya.pande@openrobotics.org, alejandro@openrobotics.org',
     description='Examples of simple launch tests',
     license='Apache License 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
         ],
